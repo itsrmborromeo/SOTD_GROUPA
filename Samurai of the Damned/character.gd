a9e14690fb022e6f772b5e0da21df03ebff46388
jrlyntp <144651413+jrlyntp@users.noreply.github.com>
2023-11-15 18:10:49 +0800

    wa na ko nalipay

---
+++ Samurai of the Damned/character.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -700.0
+@onready var ap = $AnimationPlayer
+@onready var sprite = $AnimatedSprite2D
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	#flip character
+	
+		
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+		
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if(direction != 0):
+		sprite.flip_h = (direction == -1)
+
+	move_and_slide()
