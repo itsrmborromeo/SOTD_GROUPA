6d42bc6aed3839c055b46b2621718d692128e2df
itsrmborromeo <rheamaeborromeo123@gmail.com>
2023-12-06 15:08:19 +0800

    UPDATE

--- Samurai of the Damned/scripts/skeleton_spearman.gd
+++ Samurai of the Damned/scripts/skeleton_spearman.gd
@@ -13,7 +13,7 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		#$AnimatedSprite2D.play("jump")
+		$AnimatedSprite2D.play("jump")
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
@@ -26,3 +26,8 @@ func _physics_process(delta):
 	
 
 	move_and_slide()
+
+
+
+
+
